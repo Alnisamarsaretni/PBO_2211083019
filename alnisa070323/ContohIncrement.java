e55c6ae3a53f26aefb930bf84cbd2761f58149d4
Alnisamarsaretni <marsaretnialnisa@gmail.com>
2023-03-16 12:19:40 +0700

    alnisa070323

---
+++ alnisa070323/ContohIncrement.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alnisa070323;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ContohIncrement {
+    public static void main(String[] args) {
+    int i = 10;
+    int j = 3;
+    int k = 0;
+    
+    k = ++j + i; //akan menghasilkan k = 4+10 = 14
+    
+    System.out.print(+k);
+    
+}
+}
---
+++ alnisa070323/Latihan1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alnisa070323;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+        int number = 10;
+        char letter; letter = 'a';
+        boolean result = true;
+        String str = "hello";
+        
+        System.out.println("number = "+number);
+        System.out.println("letter = "+letter);
+        System.out.println("result = "+result);
+        System.out.println("str = "+str);
+        
+        
+    }
+    
+}
